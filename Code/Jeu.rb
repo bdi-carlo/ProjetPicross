5baa0814ad4946e23a2a6b35e253fc8a337203c0
wass19 <setif7219@gmail.com>
2018-02-07 16:40:21 +0100

    modif jeu.rb et jeuxCompetetif.rb

--- Code/Jeu.rb
+++ Code/Jeu.rb
@@ -5,31 +5,46 @@
 
 class Jeu
 
-    #@temps
-    #@difficulte
-    #@pseudo
+    @temps
+    @difficulte
+    @pseudo
+    attr_accessor :pseudo,:difficulte,:taille
+
+    private_class_method :new
 
     ##
     # Constructeur qui rappelle que l'on ne peut pas instancier cette classe
     #
-    def initialize
-        raise "Erreur: classe abstraite -> impossible à instancier"
+
+    def Jeu.lancerJeu()
+        new()
+    end
+
+
+    def initialize()
+      @pseudo = setPseudo()
+      @difficulte = setDifficulte()
+      @taille = setTaille()
     end
 
     ##
     # Méthode qui permet de lancer le jeu
     #
     #
-    def lancerJeu()
-        setPseudo()
-    end
 
     ##
     # Méthode qui permet au joueur de rentrer son pseudo et le sauvegarder
     #
     def setPseudo()
-        print "Pseudo: "
-        @pseudo = gets.chomp
+      return "mehmet"
+    end
+
+    def setDifficulte()
+        return 1
+    end
+
+    def setTaille()
+      return 18
     end
 
 end
--- Code/JeuCompetetif.rb
+++ Code/JeuCompetetif.rb
@@ -1,5 +1,6 @@
 # encoding: UTF-8
-
+load 'Map.rb'
+load 'Jeu.rb'
 ##
 # Auteur  : Groupe 4
 # Fichier : JeuCompetitif.rb
@@ -7,42 +8,55 @@
 # Date : 30/01/2018
 # Cette classe représente le mode de jeu compétitif.
 
-class JeuCompetitif
+class JeuCompetitif < Jeu
 
 	@temps		#temps par partie qui va permettre de donner un score
-	@pseudo 	#pseudo du joueur
-	@difficulte	#difficulte de la partie choisie au lancement du jeu
+	#@taille
+	#@pseudo 	#pseudo du joueur
+	#@difficulte	#difficulte de la partie choisie au lancement du jeu
 	@penalite	#penalite ajouter au score apres avoir utiliser une aide
 	@score 		#score joueur
 	@highscore	#meilleur score dur joueur
-	attr_accessor :pseudo,:difficulte,:penalite,:score,:highscore
+	attr_accessor :pseudo,:difficulte,:penalite,:score,:highscore,:taille
 
 	private_class_method :new
 
-	def lancerJeu()
-			timer = Timers.new(1)
-			r = Random.new
-			r.rand(5) + 1
-			String nom = @difficulte+@taille+num
-			mapres = Map.create(@taille,@taille,nom)
-			mapres.Map.import()
-			mapres.Map.generateTop()
-			mapres.Map.generateSide()
-			map = Map.create(@taille,@taille,)
-			map.Map.import().empty()
-			timer.start()
-			while(mapres.compare(map)!=1)
-				map.display()
-			end
-			enregistreScore()
+	def JeuCompetitif.lancerJeu()
+		new()
+	end
+
+	def initialize()
+		super
+		print "oue oue oue \n"
+		print "pseudo : #{@pseudo} \n"
+		print "diff : #{@difficulte} \n"
+		print "taille : #{@taille}\n"
+		r = Random.new
+		num = r.rand(5) + 1
+		print "#{num}\n"
+		nom = "./#{@difficulte}#{@taille}#{num}"
+		map = Map.create(nom)
+		print "Chiffres du dessus :\n"
+		print "#{map.getTop}\n"
+		print "Chiffres du coté :\n"
+		print "#{map.getSide}\n"
+		print "Nombre de colonnes :\n"
+		print "#{map.getCols}\n"
+		print "Nombre de lignes :\n"
+		print "#{map.getRows}\n"
+		while(!(map.compare()))
+			map.display()
+		end
+		enregistreScore()
 	end
 
 	def enregistreScore()
-		if(finJeu==1)
-			@score = @temps + @penalite + @difficulte
-		end
+		@score = @temps + @penalite + @difficulte
 		if(@score > @highscore)
-			rentrerHighscore(pseudo,highscore)
+			print "YEAH YEAH"
 		end
 	end
 end
+
+
+j = JeuCompetitif.lancerJeu()
