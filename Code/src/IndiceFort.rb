d526dd179a7322554ac57504bf9110bd87c854a3
arthhur <arthur.prodhomme@yahoo.fr>
2018-03-02 09:48:30 +0100

    indiceFort

--- Code/src/IndiceFort.rb
+++ Code/src/IndiceFort.rb
@@ -1,19 +1,35 @@
-load "Indice.rb"
-load "Map.rb"
+load Map.rb
 
 class IndiceFort < Indice
 
+##
+# Indique si la prochaine case colorie est bonne ou non
+#
+	
+	@map
+	@colonne
+	@ligne
 
-	def initialize
+	def initialize(grille,colonne, ligne)
 		@penalites = 120
-		@map = Map.create("scenario_bateau")
+		@map = grille
+		@colonne = colonne
+		@ligne = ligne
 		@nbMax = 1
 	end
 	
-	##
-	#indique si la prochaine case colorie est bonne ou non
-	#
-	def Indice
-		
+
+##
+# Vérification de la case sur la grille
+# 
+#
+# Retourne vrai si la case sélectionnée est coloriée, faux sinon
+#
+	def envoyerIndice()
+		if(@map.accessAtRes(@ligne,@colonne))
+			return true ;
+		else 
+			return false ;
+		end
 	end
 end
